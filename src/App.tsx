74912933011439430b59e9353cd0d25e5d1fbefc
Mowalky <mowalky@outlook.com>
2022-06-28 07:40:55 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Box, Button, Text } from "@mantine/core";
+import shallow from "zustand/shallow";
+
+import { useStore } from "./store";
+
+const DoNothingButton = () => {
+  const doNothing = useStore((state) => state.doNothing);
+  return (
+    <Button size="xl" onClick={doNothing}>
+      Do Nothing
+    </Button>
+  );
+};
+
+const AddToFirstButton = () => {
+  const addToFirst = useStore((state) => state.addToFirst);
+  return (
+    <Button size="xl" onClick={addToFirst}>
+      Add To First
+    </Button>
+  );
+};
+
+const FirstValue = () => {
+  const { firstNumber } = useStore();
+  return (
+    <Text size="xl" p={5}>
+      First value: {firstNumber}
+    </Text>
+  );
+};
+
+const AddToSecondButton = () => {
+  const addToSecond = useStore((state) => state.addToSecond);
+  return (
+    <Button size="xl" onClick={addToSecond}>
+      Add To Second
+    </Button>
+  );
+};
+
+const SecondValue = () => {
+  const secondNumber = useStore((state) => state.secondNumber);
+  return (
+    <Text size="xl" p={5}>
+      Second value: {secondNumber}
+    </Text>
+  );
+};
+
+const NumbersValue = () => {
+  const numbers = useStore((state) => state.numbers, shallow);
+  return (
+    <Text size="xl" p={5}>
+      Numbers: {numbers.join(", ")}
+    </Text>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box p={10}>
+      <Box
+        sx={{
+          display: "flex",
+        }}
+      >
+        <DoNothingButton />
+      </Box>
+      <Box
+        mt={10}
+        sx={{
+          display: "flex",
+        }}
+      >
+        <AddToFirstButton />
+        <FirstValue />
+      </Box>
+      <Box
+        mt={10}
+        sx={{
+          display: "flex",
+        }}
+      >
+        <AddToSecondButton />
+        <SecondValue />
+      </Box>
+      <NumbersValue />
+    </Box>
   );
 }
 
